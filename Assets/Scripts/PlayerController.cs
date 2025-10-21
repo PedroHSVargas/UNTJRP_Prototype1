e7a9a6823ae06b7c969c5d047447e34912e8477d
Pedro Vargas <pedroh_vargas@hotmail.com>
2025-10-20 23:49:29 -0300

    Rotate the car instead just move to right. Turn variables private.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,11 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
 
-    public float forwardInput;
+    private float forwardInput;
 
     // Update is called once per frame
     void Update()
@@ -21,7 +21,8 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         // Move the vehicle forward.
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // Rotates the car based on horizontal input.
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
         
     }
